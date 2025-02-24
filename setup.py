16636cb7aa987af708d39e9c8b4ee78d3ffddf83
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2025-02-24 11:57:18 -0700

    updated version control

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 VERSION_STR_REGEX = re.compile(r'.*VERSION\s*=\s*[\'"]([^\'"]*)[\'"].*')
 
 with open('aimodellib/__init__.py', 'r', encoding='utf-8') as f:
-    version_str = f.readline().split('=')[1].strip().strip('"')
+    version_str = f.read()
     version = VERSION_STR_REGEX.match(version_str).group(1)
 
 setup(
